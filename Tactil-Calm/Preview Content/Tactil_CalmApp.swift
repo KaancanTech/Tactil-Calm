428fd06346cc1e1a5c43a061781bc3c1d86f0509
KonstancaTecj <Kaancanselen41@icloud.com>
2025-11-18 18:59:56 +0200

    push project

---
+++ Tactil-Calm/Preview Content/Tactil_CalmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tactil_CalmApp.swift
+//  Tactil-Calm
+//
+//  Created by Mixon on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Tactil_CalmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
